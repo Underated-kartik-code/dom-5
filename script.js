d3bd10621138dd0aa66708774951b220e743e0ca
Underated-kartik-code <kartikalokar8@gmail.com>
2026-02-28 21:17:08 +0530

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+let user = document.querySelector('#user');
+let pass = document.querySelector('#pass');
+let btn = document.querySelector('button');
+
+let Accounts = [
+    {user : 'durgesh',pass : 1234},
+    {user : 'kartik',pass : 1234},
+    {user : 'pranav',pass : 1234},
+    {user : 'utkarsh',pass : 1234},
+    {user : 'arpita',pass : 1234},
+    {user : 'sandhya',pass : 1234},
+]
+
+btn.addEventListener('click',function(event){
+    event.preventDefault();
+
+    let username = user.value;
+    let password = pass.value;
+
+    for(let i=0;i<Accounts.length;i++){
+        if(username == Accounts[i].user && 
+        password == Accounts[i].pass){
+            window.location.replace("home.html");
+            return;
+        }
+    }
+    console.log("imperfect")
+
+
+    let form = document.querySelector('form');
+    form.reset();
+})
